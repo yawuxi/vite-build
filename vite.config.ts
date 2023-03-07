59b18e820544abe35f42085dd1825018ccace16d
Dmytro Rylov <67982524+yawuxi@users.noreply.github.com>
2023-03-07 06:21:47 +0100

    chore: added layout alias

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       types: path.resolve("src/types"),
       page: path.resolve("src/pages"),
       service: path.resolve("src/service"),
+      layout: path.resolve("src/layout")
     },
   },
 });
